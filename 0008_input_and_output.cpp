7cd0ee7473d563c1a74392dbdc86d3b93ec86583
massimo <massimo@info.nl>
2019-09-19 13:59:56 +0200

    Input output exercise

---
+++ 0008_input_and_output.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Massimo Biancalani on 2019-09-19.
+//
+
+#include <iostream>
+
+using namespace std;
+void sum3Numbers() {
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+    int soluton = n1 + n2 + n3;
+    cout << soluton;
+}
